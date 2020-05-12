19911ef7b6659d3dd91e4f10fb8cf99f915cbfd8
James Pierson <jameswpbusiness@gmail.com>
2020-05-11 23:39:37 -0400

    Updates

---
+++ precache-manifest.2043f6769c1a683d6582e98a6a68ca60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4fea11a061cdb6f519a5b2251de1f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc9c6b671bf4d1457f7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "29202082136bbf00760e",
+    "url": "/static/js/2.f029e917.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f029e917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc9c6b671bf4d1457f7",
+    "url": "/static/js/main.b5a02836.chunk.js"
+  },
+  {
+    "revision": "a8f97462f4135fc3941b",
+    "url": "/static/js/runtime-main.39142cf7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
